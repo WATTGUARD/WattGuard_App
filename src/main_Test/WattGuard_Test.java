9db30c2400310c8136c9ff71bc0fcb42f21c3549
hannndii <shiirogami95@gmail.com>
2024-12-13 15:42:02 +0700

    update

--- src/main_Test/WattGuard_Test.java
+++ src/main_Test/WattGuard_Test.java
@@ -1,21 +1,58 @@
 package main_Test;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in th
-import java.util.ArrayList;
-import Token.Token;
-import Token.Tagihan;
+import java.util.Scanner;
+import Informasi.Cha;
 
 public class WattGuard_Test {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        ArrayList<Token> daftarToken = new ArrayList<>();
 
-        daftarToken.add(new Token(50000, 1234567890123456L, 30.0, "Transfer Bank"));
-        daftarToken.add(new Token(100000, 1234567890123457L, 60.0, "Transfer Bank"));
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("\n=== MENU UTAMA ===");
+            System.out.println("1. Layanan Pengguna");
+            System.out.println("2. FAQ");
+            System.out.println("3. Isi Token");
+            System.out.println("4. Riwayat Pembelian");
+            System.out.println("5. Notifikasi");
+            System.out.println("0. Keluar");
 
-        for (Token token : daftarToken) {
-            System.out.println(token);
+            System.out.println("Pilih(1-6): ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 2:
+                    System.out.println("Anda memilih Layanan Pengguna.");
+                    // Tambahkan logika untuk Layanan Pengguna di sini
+                    Chatbot
+                    break;
+                case 3:
+                    System.out.println("Anda memilih FAQ.");
+                    // Tambahkan logika untuk FAQ di sini
+                    break;
+                case 4:
+                    System.out.println("Anda memilih Isi Token.");
+                    // Tambahkan logika untuk Isi Token di sini
+                    break;
+                case 5:
+                    System.out.println("Anda memilih Notifikasi.");
+                    // Tambahkan logika untuk Notifikasi di sini
+                    break;
+                case 6:
+                    System.out.println("Anda memilih Statistik.");
+                    // Tambahkan logika untuk Statistik di sini
+                    break;
+                case 0:
+                    System.out.println("Keluar dari program. Terima kasih.");
+                    return;
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan coba lagi.");
+            }
         }
+
+
+
 
     }
 }
